db594090d4b1d7ad75f09b182be9bbd5e3c6a383
Wenceslao <wenceslaomateos@gmail.com>
2019-09-06 19:00:32 -0300

    cambios menores

--- subida-db.php
+++ subida-db.php
@@ -6,11 +6,10 @@ $longitud = $_REQUEST['longitud'];
 $latitud = $_REQUEST['latitud'];
 $filename = $_FILES['adjunto']['tmp_name'];
 
-$newFileName = $nombre . $apellido;
-exec("mv '$filename' fotos/$newFileName");
+exec("mv '$filename' fotos/$filename");
 require('coneccion.php');
 $query = "  INSERT INTO sociedad (foto, posicion, nombre, apellido, descripcion) 
-                VALUES ('$newFileName', ST_GeomFromText('POINT($longitud $latitud)', 4326), '$nombre', '$apellido', '$descripcion');";
+                VALUES ('$filename', ST_GeomFromText('POINT($longitud $latitud)', 4326), '$nombre', '$apellido', '$descripcion');";
 echo $query;
 mysqli_query($db, $query);
 header("location: visualizacion.php");
