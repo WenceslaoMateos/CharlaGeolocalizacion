4d3a28873469c1696d99212636fe7c49f3564853
Wenceslao <wenceslaomateos@gmail.com>
2019-09-06 18:59:44 -0300

    subida de datos completa

--- subida-db.php
+++ subida-db.php
@@ -1,15 +1,16 @@
 <?php
-    $nombre = $_REQUEST['nombre'];
-    $apellido= $_REQUEST['apellido'];
-    $descripcion= $_REQUEST['descripcion'];
-    $longitud = $_REQUEST['longitud'];
-    $latitud = $_REQUEST['latitud'];
-    $filename = $_FILES['adjunto']['tmp_name'];
+$nombre = $_REQUEST['nombre'];
+$apellido = $_REQUEST['apellido'];
+$descripcion = $_REQUEST['descripcion'];
+$longitud = $_REQUEST['longitud'];
+$latitud = $_REQUEST['latitud'];
+$filename = $_FILES['adjunto']['tmp_name'];
 
-    $newFileName = $nombre . $apellido;
-    exec("mv '$filename' fotos/$newFileName");
-    require('coneccion.php');
-    $query = "  INSERT INTO sociedad (foto, posicion, nombre, apellido, descripcion) 
+$newFileName = $nombre . $apellido;
+exec("mv '$filename' fotos/$newFileName");
+require('coneccion.php');
+$query = "  INSERT INTO sociedad (foto, posicion, nombre, apellido, descripcion) 
                 VALUES ('$newFileName', ST_GeomFromText('POINT($longitud $latitud)', 4326), '$nombre', '$apellido', '$descripcion');";
-    echo $query;
-    mysqli_query($db, $query);
+echo $query;
+mysqli_query($db, $query);
+header("location: visualizacion.php");
