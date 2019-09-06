a19814ae9eef5dca1bce36b4676cf8f7bebd15ac
Wenceslao <wenceslaomateos@gmail.com>
2019-09-06 20:01:36 -0300

    Datos que no pertenecen

--- visualizacion.php
+++ visualizacion.php
@@ -30,8 +30,6 @@ $resultado .= "]";
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Visualizacion</title>
     <script src="./jquery/jquery.min.js"></script>
     <link href="./bootstrap/css/bootstrap.min.css" rel="stylesheet">
